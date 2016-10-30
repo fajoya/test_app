575ba3d022b9b352fabf975338a09f4f77a88ab2
Francisco Joya <Francisco Joya>
2016-10-30 04:25:39 -0500

    Created dock model

---
+++ app/views/docks/_dock.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! dock, :id, :name, :created_at, :updated_at
+json.url dock_url(dock, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :docks
   resources :workers
   root 'workers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
